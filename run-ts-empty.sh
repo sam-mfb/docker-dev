a92d97f39f42f075d9db25c0ae362f5ae018872a
Sam Davidoff <sam@mfbtech.com>
2022-03-05 02:30:11 -0500

    give ts dev empty non-conflicting names

--- run-ts-empty.sh
+++ run-ts-empty.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
 IMAGE_TARGET="ts-dev"
-IMAGE_TAG="ts-dev"
-CONTAINER_NAME="ts-dev-active"
+IMAGE_TAG="ts-dev-empty"
+CONTAINER_NAME="ts-dev-empty-active"
 DOCKER_USER_HOME="/home/devuser"
-HOST_PORTS="3000"
+HOST_PORTS="3010"
 CONTAINER_PORTS="3000"
 HOSTNAME="ts-docker-empty"
 GIT_REPO=""
