e0a02ead5003e0542d993e24629ea05e08d44d5e
Sam Davidoff <sam@mfbtech.com>
2022-12-22 12:40:49 -0500

    update seccomp profile to allow microsoft graph use in powershell

--- run-func.sh
+++ run-func.sh
@@ -37,7 +37,7 @@ if [[ "$(docker container ls -qa --filter name=${CONTAINER_NAME} 2> /dev/null)" 
     #  - set detach key to ctrl z,z to free up ctrl,p (the default)
     #  - set DISPLAY env so we can use XServer over the network
     #  - use a custome seccomp profile that enables chrome sandbox
-    docker run -p ${HOST_PORTS}:${CONTAINER_PORTS} --mount type=bind,src=${HOME}/.ssh,target=${DOCKER_USER_HOME}/.ssh --mount type=bind,src=/var/run/docker.sock,target=/var/run/docker.sock --shm-size=2gb --detach-keys='ctrl-z,z' --name ${CONTAINER_NAME} -e DISPLAY=host.docker.internal:0 --security-opt seccomp=chrome.json -h ${HOSTNAME} -it ${IMAGE_TAG}
+    docker run -p ${HOST_PORTS}:${CONTAINER_PORTS} --mount type=bind,src=${HOME}/.ssh,target=${DOCKER_USER_HOME}/.ssh --mount type=bind,src=/var/run/docker.sock,target=/var/run/docker.sock --shm-size=2gb --detach-keys='ctrl-z,z' --name ${CONTAINER_NAME} -e DISPLAY=host.docker.internal:0 --security-opt seccomp=custom-seccomp.json -h ${HOSTNAME} -it ${IMAGE_TAG}
 else
     echo "Starting and attaching to existing container..."
     docker start --detach-keys='ctrl-z,z' -i ${CONTAINER_NAME}
