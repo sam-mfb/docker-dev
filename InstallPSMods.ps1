22946b3248871df0701342124655e9b0f5cdbb6e
Sam Davidoff <sam@mfbtech.com>
2022-12-22 09:34:02 -0500

    add MS Graph powershell module

--- InstallPSMods.ps1
+++ InstallPSMods.ps1
@@ -3,4 +3,5 @@ Install-Module -Name SqlServer
 Install-Module -Name AzureAd
 Install-Module -Name Az.Accounts
 Install-Module -Name Az.Sql
+Install-Module -Name Microsoft.Graph
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
