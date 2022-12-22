3658d311ff07936da0ae931041c2d8502088e675
Sam Davidoff <sam@mfbtech.com>
2022-12-22 18:06:48 -0500

    change installed PS modules

--- InstallPSMods.ps1
+++ InstallPSMods.ps1
@@ -1,7 +1,5 @@
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module -Name SqlServer
-Install-Module -Name AzureAd
-Install-Module -Name Az.Accounts
-Install-Module -Name Az.Sql
+Install-Module -Name Az
 Install-Module -Name Microsoft.Graph
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
