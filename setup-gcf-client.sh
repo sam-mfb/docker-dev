61a4802bcb45822d4267f22ce7d60757493f6d9b
Sam Davidoff <sam@mfbtech.com>
2024-04-25 13:22:42 -0400

    update setup script to overwrite vscode settings

--- setup-gcf-client.sh
+++ setup-gcf-client.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-git config --global credential.helper '!f(){ node ~/gcf-client.js $*; }; f'
+# this line is needed to overwrite vscode which writes its helper to both places
+sudo git config --system --replace-all credential.helper '!f(){ node ~/gcf-client.js $*; }; f'
+
+git config --global --replace-all credential.helper '!f(){ node ~/gcf-client.js $*; }; f'
 git config --global credential.https://dev.azure.com.useHttpPath true
